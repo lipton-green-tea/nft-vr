1de43c1ee72b181bef0afe38f0f39e33b438cdd0
Robert Stok <rs.stok@gmail.com>
2022-01-23 01:49:33 +0000

    added teleportation

---
+++ client/components/input-listen.js
@@ -0,0 +1,13 @@
+AFRAME.registerComponent("input-listen", {
+    init: function () {
+        //X-button Pressed
+        this.el.addEventListener("xbuttondown", function (e) {
+            this.emit("teleportstart");
+        });
+
+        //X-button Released
+        this.el.addEventListener("xbuttonup", function (e) {
+            this.emit("teleportend");
+        });
+    }
+});
